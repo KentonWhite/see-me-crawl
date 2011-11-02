c14536ae92db10e09cd2ecb33eac8fcf10199bb9
KentonWhite <jkentonwhite@gmail.com>
2011-11-02 15:15:53 -0400

    Implemented MarkovChain#select_candidate

--- lib/markov_chain.rb
+++ lib/markov_chain.rb
@@ -1,7 +1,21 @@
+
 class MarkovChain
+  class NoValidNextNode < StandardError; end
+
   def next(current_node)
     candidate_node = select_candidate(current_node)
     rand <= acceptance_probability(current_node, candidate_node) ?
       candidate_node : current_node
   end
+  
+  def select_candidate(current_node)
+    nodes = current_node.connections
+    nodes.shuffle!
+    begin
+      raise NoValidNextNode if nodes.empty?
+      candidate_node = current_node.new_node(nodes.shift) 
+    end while candidate_node.private?
+    candidate_node
+  end
+  
 end
--- spec/lib/markov_chain_spec.rb
+++ spec/lib/markov_chain_spec.rb
@@ -13,10 +13,43 @@ describe MarkovChain do
       @mc.stub!(:acceptance_probability).and_return(1)
       @mc.next(@current_node).should == @candidate_node
     end
-    
+
     it 'should return the current node' do
       @mc.stub!(:acceptance_probability).and_return(0)
       @mc.next(@current_node).should == @current_node
+    end
+  end
+
+  describe 'select_candidate' do
+    before :each do
+      @mc = MarkovChain.new
+      @current_node = mock(:BaseNode)
+      @public_node = mock(:PublicNode)
+      @public_node.stub!(:private?).and_return(false)
+      @private_node = mock(:PrivateNode)
+      @private_node.stub!(:private?).and_return(true)
+      nodes = (1..10).map { @private_node }
+      nodes << @public_node 
+      @current_node.stub!(:connections).and_return(nodes)
+      @current_node.stub!(:new_node).with(@public_node).and_return(@public_node)
+      @current_node.stub!(:new_node).with(@private_node).and_return(@private_node) 
+    end 
+    
+    it 'should return a node object' do
+      @mc.select_candidate(@current_node).class.should == @current_node.class
+    end
+
+    it 'should select public node' do
+      @mc.select_candidate(@current_node).should == @public_node
+    end 
+
+    it 'should not select private node' do
+      @mc.select_candidate(@current_node).should_not == @private_node
+    end
+
+    it 'should throw an error' do
+      @current_node.stub!(:connections).and_return([])
+      expect { @mc.select_candidate(@current_node) }.to raise_error(MarkovChain::NoValidNextNode)
     end
   end
 end
