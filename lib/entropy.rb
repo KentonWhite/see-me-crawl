f3e0bcb15312fbcc37344e485a8f7655420d3996
Kenton White <jkentonwhite@gmail.com>
2012-06-12 17:28:07 -0400

    Implemented Entropy class
    Computes entropy on the sample

--- lib/entropy.rb
+++ lib/entropy.rb
@@ -1,19 +1,39 @@
 require 'dm-chunked_query'
 
 class Entropy
-  attr_accessor :distribution
+  attr_accessor :distribution, :total_degree
   
   def initialize
     @distribution = Hash.new(0)
+    @total_degree = 0
+    load_distribution
   end
   
   def load_distribution
     DataMapper.repository(:local) do
       Sample.each_chunk(20) do |chunk|
         chunk.each do |s|
-          @distribution[s.degree] += 1
+          add_node s
         end
       end
     end
+  end
+  
+  def add_node(node)
+    @distribution[node.degree] += 1
+    @total_degree += node.degree
+  end
+  
+  def q(degree)
+    degree*@distribution[degree]/total_degree.to_f
+  end
+  
+  def entropy
+    @distribution.reduce(0) { |h, (degree, n)| qi = degree*n/total_degree.to_f; h - qi*Math.log(qi) }
+  end
+  
+  def entropy!(node)
+    add_node(node)
+    entropy
   end
 end
--- spec/lib/entropy_spec.rb
+++ spec/lib/entropy_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper.rb'
 require './lib/entropy.rb'
 require './lib/sample.rb'
+require './lib/graph.rb'
+require './lib/base_node'
 
 describe Entropy do
   before :each do
@@ -31,6 +33,56 @@ describe Entropy do
     it 'should load the distribution' do
       @entropy.load_distribution
       @entropy.distribution.should == {2 => 5, 5 => 3}
+    end
+  end
+  
+  describe 'add_node' do
+    before :each do
+      @node = BaseNode.new(Factory(:node).id)
+      @entropy.add_node @node
+    end
+    it 'should add a node to the distribution' do
+      @entropy.distribution[@node.degree].should == 1
+    end
+    
+    it 'should update the total degree' do
+      @entropy.total_degree.should == @node.degree
+    end
+  end
+  
+  describe 'total_degree' do
+    before :each do
+      (1..5).each { |i| Factory(:sample, degree: 2) }
+      (1..3).each { |i| Factory(:sample, degree: 5) }
+      @entropy = Entropy.new
+    end
+    
+    it 'should return a total degree of 25' do
+      @entropy.total_degree.should == 25
+    end
+  end
+  
+  describe 'q' do
+    before :each do
+      (1..5).each { |i| Factory(:sample, degree: 2) }
+      (1..3).each { |i| Factory(:sample, degree: 5) }
+      @entropy = Entropy.new
+    end 
+    
+    it 'should return a q value for the degree value' do
+      @entropy.q(5).should == 0.6
+    end   
+  end
+  
+  describe 'entropy' do
+    before :each do
+      (1..5).each { |i| Factory(:sample, degree: 2) }
+      (1..3).each { |i| Factory(:sample, degree: 5) }
+      @entropy = Entropy.new
+    end 
+    
+    it 'should return the entropy of the sampled graph' do
+      @entropy.entropy.round(9).should == 0.673011667
     end
   end
 end
