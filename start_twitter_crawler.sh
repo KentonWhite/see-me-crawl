49e00faffbac0724cdf275f4c9f3bec27b40e5ed
Kenton White <jkentonwhite@gmail.com>
2012-06-11 22:52:33 -0400

    Fixed log file path

--- start_twitter_crawler.sh
+++ start_twitter_crawler.sh
@@ -1,3 +1,3 @@
 crontab -r
 crontab /home/ubuntu/see-me-crawl/cron.txt
-unbuffer ruby /home/ubuntu/see-me-crawl/twitter_crawler.rb >> log.txt &
+unbuffer ruby /home/ubuntu/see-me-crawl/twitter_crawler.rb >> /home/ubuntu/see-me-crawl/log.txt &
