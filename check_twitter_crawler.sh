ae11ff8fba8dd29b2fd16dc91f179024777c44b0
Kenton White <jkentonwhite@gmail.com>
2012-06-11 22:37:12 -0400

    Improved check for empty line

--- check_twitter_crawler.sh
+++ check_twitter_crawler.sh
@@ -1,6 +1,3 @@
 ruby_job=`ps aux | grep ruby | grep -v grep`
 echo $ruby_job
-if [ "$ruby_job" == '' ]
-	then
-	`/home/ubuntu/see-me-crawl/start_twitter_crawler.sh`
-fi
+[ -z "$run_job" ] && `/home/ubuntu/see-me-crawl/start_twitter_crawler.sh`
