4642da59903940cd9cf4b88e7d4f818fd161d850
Kenton White <jkentonwhite@gmail.com>
2012-06-13 20:22:42 -0400

    Check candidate_node.degree == 0 before division

--- lib/metropolis_hastings_markov_chain.rb
+++ lib/metropolis_hastings_markov_chain.rb
@@ -1,6 +1,7 @@
 require './lib/markov_chain.rb'
 class MetropolisHastingsMarkovChain < MarkovChain 
   def acceptance_probability(current_node, candidate_node)
+    return 1.0 if candidate_node.degree == 0
     [1.0, current_node.degree.to_f/candidate_node.degree.to_f].min
   end
 end
