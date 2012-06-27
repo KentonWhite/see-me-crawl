308a3ddb5aa14f248c1ce94a689ba7974eea6dd1
Kenton White <jkentonwhite@gmail.com>
2012-06-27 10:54:14 -0400

    Catch SSL errors

--- lib/twitter_node.rb
+++ lib/twitter_node.rb
@@ -26,7 +26,7 @@ class TwitterNode < BaseNode
     until cursor == 0
       begin
         result = client.send("#{type.to_s.singularize}_ids", id, cursor: cursor)
-      rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, SocketError, EOFError => e 
+      rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, OpenSSL::SSL::SSLError, SocketError, EOFError => e 
         p e.message
         retry
       end
@@ -63,7 +63,7 @@ class TwitterNode < BaseNode
       @in_degree = user.followers_count
       @out_degree = user.friends_count
       @private = user.protected
-    rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, SocketError, EOFError => e 
+    rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, OpenSSL::SSL::SSLError, SocketError, EOFError => e 
       p e.message
       retry
     rescue Twitter::Forbidden, Twitter::NotFound  => e
