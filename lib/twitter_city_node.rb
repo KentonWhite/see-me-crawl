c5e980fa8cb8b294a4a1b599c51b6e4ce51cd0b2
Kenton White <jkentonwhite@gmail.com>
2012-07-11 11:41:37 -0400

    Rescue twitter errors when looking up location

--- lib/twitter_city_node.rb
+++ lib/twitter_city_node.rb
@@ -14,7 +14,12 @@ class TwitterCityNode < TwitterNode
    nodes = super(type)
    filtered_nodes = []
    nodes.each_slice(100) do |n|
-     users = client.users(n)
+     begin
+       users = client.users(n)
+     rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, OpenSSL::SSL::SSLError, SocketError, EOFError => e 
+       p e.message
+       retry
+     end
      users.keep_if { |u| u.location =~ /#{@@city}/i }
      filtered_nodes.concat users.map { |u| u.id }
    end 
