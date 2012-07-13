dc6493e7177dd220e6ae36bb9b8ad72421d1d127
Kenton White <jkentonwhite@gmail.com>
2012-07-13 13:01:46 -0400

    Sample hashtags

---
+++ lib/hashtag_twitter_node.rb
@@ -0,0 +1,31 @@
+require './lib/twitter_node.rb' 
+
+class HashtagTwitterNode < TwitterNode
+  
+  @@hashtag_regex = /#iunfollowedyoubecause/i
+
+  def hashtag?
+    @hastag ||= check_hashtag
+  end
+  
+  private
+  
+  def check_hashtag
+    puts "Checking for hashtag...."
+    begin
+      statuses = client.user_timeline(id, :count  => 200, :include_rts => true)
+    rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, OpenSSL::SSL::SSLError, SocketError, EOFError, Zlib::GzipFile::Error => e 
+      p e.message
+      retry
+    end
+    statuses.each do |s|
+      if s.text =~ @@hashtag_regex then
+        puts "hashtag found"
+        puts s.text
+        return 1
+      end
+    end
+    puts "hashtag not found"
+    return 0
+  end
+end
