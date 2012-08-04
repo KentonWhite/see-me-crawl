e6d61387d697790d17c14c677192772f12034ae0
Kenton White <jkentonwhite@gmail.com>
2012-08-04 13:51:02 +1000

    Added Faraday Timeout Error

--- lib/twitter_node.rb
+++ lib/twitter_node.rb
@@ -28,7 +28,7 @@ class TwitterNode < BaseNode
     until cursor == 0
       begin
         result = client.send("#{type.to_s.singularize}_ids", id, cursor: cursor)
-      rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, OpenSSL::SSL::SSLError, SocketError, EOFError, Errno::ETIMEDOUT, Zlib::GzipFile::Error, Timeout::Error => e 
+      rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, OpenSSL::SSL::SSLError, SocketError, EOFError, Errno::ETIMEDOUT, Zlib::GzipFile::Error, Timeout::Error, Faraday::Error::TimeoutError => e 
         p client
         p e.message
         retry
@@ -66,7 +66,7 @@ class TwitterNode < BaseNode
       @in_degree = user.followers_count
       @out_degree = user.friends_count
       @private = user.protected
-    rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, OpenSSL::SSL::SSLError, SocketError, EOFError, Errno::ETIMEDOUT, Zlib::GzipFile::Error, Timeout::Error => e 
+    rescue Twitter::ServiceUnavailable, Errno::ECONNRESET, Twitter::BadGateway, Twitter::BadRequest, Twitter::InternalServerError, OpenSSL::SSL::SSLError, SocketError, EOFError, Errno::ETIMEDOUT, Zlib::GzipFile::Error, Timeout::Error, Faraday::Error::TimeoutError => e 
       p e.message
       retry
     rescue Twitter::Forbidden, Twitter::NotFound  => e
