299feaa8d35ad99e14ecc70dd84e3b2122b67cdb
Kenton White <jkentonwhite@gmail.com>
2012-08-23 12:32:36 -0400

    Switched to use first_or_new

--- rakefile.rb
+++ rakefile.rb
@@ -7,8 +7,8 @@ task :cron do
   DataMapper.setup(:default, adapter: 'mysql', database: 'sample', user: 'root')
   summaries = repository(:default).adapter.select("SELECT COUNT(DISTINCT s.id) AS count, m.message_date AS date FROM samples AS s INNER JOIN messages AS m ON s.node = m.node GROUP BY date")
   summaries.each do |s|
-    summary = Summary.first_or_create(date: s.date)
-    summary.attributes = {count: s.count}
+    summary = Summary.first_or_new(date: s.date)
+    summary.count = s.count
     summary.save
   end
   log.info("End summaries")
