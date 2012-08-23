cddfbf7c33c0356d83565bf31acdee596cb9aba6
Kenton White <jkentonwhite@gmail.com>
2012-08-23 11:07:36 -0400

    Setup DataMapper Adapter

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,7 @@
 require './lib/sample.rb'
 
 task :cron do
+  DataMapper.setup(:default, adapter: 'mysql', database: 'sample', user: 'root')
   summaries = repository(:default).adapter.select("SELECT COUNT(DISTINCT s.id) AS count, m.message_date AS date FROM samples AS s INNER JOIN messages AS m ON s.node = m.node GROUP BY date")
   p summaries
     
