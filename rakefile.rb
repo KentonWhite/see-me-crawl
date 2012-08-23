ca808e44d05ba9dc5728e7c107e0fb9df9ae291b
Kenton White <jkentonwhite@gmail.com>
2012-08-23 12:02:14 -0400

    Use find_or_create to update summaries

--- rakefile.rb
+++ rakefile.rb
@@ -7,7 +7,9 @@ task :cron do
   DataMapper.setup(:default, adapter: 'mysql', database: 'sample', user: 'root')
   summaries = repository(:default).adapter.select("SELECT COUNT(DISTINCT s.id) AS count, m.message_date AS date FROM samples AS s INNER JOIN messages AS m ON s.node = m.node GROUP BY date")
   summaries.each do |s|
-    Summary.first_or_create(date: s.date, )
+    summary = Summary.first_or_create(date: s.date)
+    summary.attributes = {count: s.count}
+    summary.save
   end
   log.info("End summaries")
 end
