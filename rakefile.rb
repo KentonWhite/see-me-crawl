a08fe77e537aa3a39d0b46d2dc33dadfd39054b8
Kenton White <jkentonwhite@gmail.com>
2012-08-30 15:50:13 -0400

    All migrations together

--- rakefile.rb
+++ rakefile.rb
@@ -21,45 +21,47 @@ end
 task :migrate_to_counts do
   log.info("Start migration")
   
-  # log.info("Running hashtag migration")
-  # 
-  # hashtags = repository(:default).adapter.select("SELECT DISTINCT(hashtag) as hashtag from hashtags")
-  # hashtags.each do |h|
-  #   log.info("Processing hashtag #{h} -- Standard Counts")
-  #   counts = repository(:default).adapter.select("SELECT COUNT(DISTINCT(node)) as count, hashtag_date as date from hashtags where hashtag = '#{h}' GROUP BY date")
-  #   counts.each do |c|
-  #     item = HashtagCount.new(hashtag: h, date: c.date, count: c.count)
-  #     log.info(item)
-  #     item.save
-  #   end
-  #   
-  #   log.info("Processing hashtag #{h} -- MH Counts")
-  #   counts = repository(:default).adapter.select("SELECT COUNT(DISTINCT(s.id)) as count, h.hashtag_date as date from samples AS s INNER JOIN hashtags AS h ON s.node = h.node where hashtag = '#{h}' GROUP BY date")
-  #   counts.each do |c|
-  #     item = HashtagMhCount.new(hashtag: h, date: c.date, count: c.count)
-  #     log.info(item)
-  #     item.save
-  #   end
+  log.info("Running hashtag migration")
+  
+  hashtags = repository(:default).adapter.select("SELECT DISTINCT(hashtag) as hashtag from hashtags")
+  hashtags.each do |h|
+    log.info("Processing hashtag #{h} -- Standard Counts")
+    counts = repository(:default).adapter.select("SELECT COUNT(DISTINCT(node)) as count, hashtag_date as date from hashtags where hashtag = '#{h}' GROUP BY date")
+    counts.each do |c|
+      item = HashtagCount.new(hashtag: h, date: c.date, count: c.count)
+      log.info(item)
+      item.save
+    end
     
-  # log.info("Running mention migration")
-  # 
-  # mentions = repository(:default).adapter.select("SELECT DISTINCT(mention) as mention from mentions")
-  # mentions.each do |m|
-  #   log.info("Processing mention #{m} -- Standard Counts")
-  #   counts = repository(:default).adapter.select("SELECT COUNT(DISTINCT(node)) as count, mention_date as date from mentions where mention = '#{m}' GROUP BY date")
-  #   counts.each do |c|
-  #     item = MentionCount.new(mention: m, date: c.date, count: c.count)
-  #     log.info(item)
-  #     item.save
-  #   end
-  #   
-  #   log.info("Processing mention #{m} -- MH Counts")
-  #   counts = repository(:default).adapter.select("SELECT COUNT(DISTINCT(s.id)) as count, m.mention_date as date from samples AS s INNER JOIN mentions AS m ON s.node = m.node where mention = '#{m}' GROUP BY date")
-  #   counts.each do |c|
-  #     item = MentionMhCount.new(mention: m, date: c.date, count: c.count)
-  #     log.info(item)
-  #     item.save
-  #   end
+    log.info("Processing hashtag #{h} -- MH Counts")
+    counts = repository(:default).adapter.select("SELECT COUNT(DISTINCT(s.id)) as count, h.hashtag_date as date from samples AS s INNER JOIN hashtags AS h ON s.node = h.node where hashtag = '#{h}' GROUP BY date")
+    counts.each do |c|
+      item = HashtagMhCount.new(hashtag: h, date: c.date, count: c.count)
+      log.info(item)
+      item.save
+    end
+  end
+    
+  log.info("Running mention migration")
+  
+  mentions = repository(:default).adapter.select("SELECT DISTINCT(mention) as mention from mentions")
+  mentions.each do |m|
+    log.info("Processing mention #{m} -- Standard Counts")
+    counts = repository(:default).adapter.select("SELECT COUNT(DISTINCT(node)) as count, mention_date as date from mentions where mention = '#{m}' GROUP BY date")
+    counts.each do |c|
+      item = MentionCount.new(mention: m, date: c.date, count: c.count)
+      log.info(item)
+      item.save
+    end
+    
+    log.info("Processing mention #{m} -- MH Counts")
+    counts = repository(:default).adapter.select("SELECT COUNT(DISTINCT(s.id)) as count, m.mention_date as date from samples AS s INNER JOIN mentions AS m ON s.node = m.node where mention = '#{m}' GROUP BY date")
+    counts.each do |c|
+      item = MentionMhCount.new(mention: m, date: c.date, count: c.count)
+      log.info(item)
+      item.save
+    end
+  end
     
   log.info("Running message migration")
   
