caf40622922e3e0962613a9f4c39938310d334bf
Kenton White <jkentonwhite@gmail.com>
2012-08-30 14:47:25 -0400

    Simple test counts of messages

--- rakefile.rb
+++ rakefile.rb
@@ -3,11 +3,11 @@ require 'logger'
 log = Logger.new(STDOUT)
 log.level = Logger::INFO
 
+DataMapper.setup(:default, adapter: 'mysql', database: 'sample', user: 'root')
+DataMapper.auto_upgrade!
+
 task :cron do
   log.info("Start summaries")
-  
-  DataMapper.setup(:default, adapter: 'mysql', database: 'sample', user: 'root')
-  DataMapper.auto_upgrade!
   
   summaries = repository(:default).adapter.select("SELECT COUNT(DISTINCT s.id) AS count, m.message_date AS date FROM samples AS s INNER JOIN messages AS m ON s.node = m.node GROUP BY date")
   summaries.each do |s|
@@ -16,4 +16,29 @@ task :cron do
     summary.save
   end
   log.info("End summaries")
+end
+
+task :migrate_to_counts do
+  log.info("Start migration")
+  
+  log.info("Running hashtag migration")
+  
+  Hashtag.each_chunk(100) do |hashtags|
+    hashtags.each do |h|
+      item = HashtagCount.first_or_new({hashtag: h.hashtag, date: h.hashtag_date}, {count: 0})
+      item.count += 1
+      log.info(item)
+      item.save 
+      
+      item = HashtagMhCount.first_or_new({hashtag: h.hashtag, date: h.hashtag_date}, {count: 0})
+      item.count += 1
+      log.info(item)
+      item.save 
+    end
+  end 
+  
+  log.info("Hastag migation complete")
+  
+  log.info("End migration")
+  
 end
