36c68fd964a7659ab07deaec4844fc145386ee2f
Kenton White <jkentonwhite@gmail.com>
2012-09-05 15:55:18 -0400

    Created separate process to process messages

---
+++ process_new_messages.rb
@@ -0,0 +1,82 @@
+require './lib/sample.rb'
+require './lib/tag_counter'
+require 'logger'
+require 'dm-aggregates'
+log = Logger.new(STDOUT)
+log.level = Logger::INFO
+
+DataMapper.setup(:default, adapter: 'mysql', database: 'sample', user: 'root')
+DataMapper.auto_upgrade!
+
+log.info("Start message processing queue")
+  
+while(true) do
+  
+  counter = TagCounter.new
+  
+  
+  hashtags = UnprocessedHashtag.all
+  log.info("Processing #{hashtags.count} hashtags")
+  
+  hashtags.each do |h|
+    counter.add(h.hashtag_date, h.hashtag, h.node)
+    attrs = h.attributes
+    attrs.delete(:id)
+    Hashtag.create(attrs)    
+    h.destroy
+  end
+  
+  counter.each do |date, tag, nodes|
+    hashtag_count = HashtagCount.first_or_new(date: date, hashtag: tag)
+    hashtag_count.count = nodes.count
+    hashtag_mh_count = HashtagMhCount.first_or_new(date: date, hashtag: tag)
+    hashtag_mh_count.count = Sample.count(node: nodes.to_a)
+    hashtag_count.save
+    hashtag_mh_count.save
+  end
+  
+    
+  mentions = UnprocessedMention.all
+  log.info("Processing #{mentions.count} mentions")
+  
+  mentions.each do |m|
+    counter.add(m.mention_date, m.mention, m.node)
+    attrs = m.attributes
+    attrs.delete(:id)
+    Mention.create(attrs)    
+    m.destroy
+  end
+  
+  counter.each do |date, tag, nodes|
+    mention_count = MentionCount.first_or_new(date: date, mention: tag)
+    mention_count.count = nodes.count
+    mention_mh_count = MentionMhCount.first_or_new(date: date, mention: tag)
+    mention_mh_count.count = Sample.count(node: nodes.to_a)
+    mention_count.save
+    mention_mh_count.save
+  end
+      
+  messages = UnprocessedMessage.all
+  log.info("Processing #{messages.count} messages")
+  
+  messages.each do |m|
+    counter.add(m.message_date, 'message', m.node)
+    attrs = m.attributes
+    attrs.delete(:id)
+    Message.create(attrs)    
+    m.destroy
+  end
+  
+  counter.each do |date, tag, nodes|
+    message_count = MessageCount.first_or_new(date: date)
+    message_count.count = nodes.count
+    message_mh_count = MentionMhCount.first_or_new(date: date)
+    message_mh_count.count = Sample.count(node: nodes.to_a)
+    message_count.save
+    message_mh_count.save
+  end
+  
+end
+  
+log.info("Ending message processing queue")
+ 
--- start_twitter_crawler.sh
+++ start_twitter_crawler.sh
@@ -1,1 +1,2 @@
 unbuffer ruby ./twitter_crawler.rb --memcached >> log.txt &
+unbuffer ruby ./process_new_messages.rb >> log.txt &
