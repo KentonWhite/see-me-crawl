f1fb05523608e75bcd59a04efa5dbb844b16ed74
Kenton White <jkentonwhite@gmail.com>
2012-09-14 13:27:51 -0400

    Added process hashtags

--- message_processor.rb
+++ message_processor.rb
@@ -21,6 +21,9 @@ AMQP.start('amqp://lcpdyzjs:nko1XmnZfRul4Hza@gqezbdhq.heroku.srs.rabbitmq.com:21
       unless UnprocessedMessage.count(node: node) > 0 
         node = HashtagTwitterNode.new(node.to_i)
         node.check_hashtag
+        hashtags = UnprocessedHashtag.all
+        process_hashtags if hashtags.count > 1000
+
       end
     else
       puts "Unknown message type #{metadata.type}"
@@ -30,5 +33,31 @@ AMQP.start('amqp://lcpdyzjs:nko1XmnZfRul4Hza@gqezbdhq.heroku.srs.rabbitmq.com:21
   end
   
   puts "Ready to listen to messages queue"
+  STDOUT.flush
+end
+
+def process_hashtags(hashtags)
+  counter = TagCounter.new
+  
+  puts "Processing #{hashtags.count} hashtags"
+  STDOUT.flush
+  
+  hashtags.each do |h|
+    counter.add(h.hashtag_date, h.hashtag, h.node)
+    attrs = h.attributes
+    attrs.delete(:id)
+    Hashtag.create(attrs)    
+    h.destroy
+  end
+  
+  counter.each do |date, tag, nodes|
+    hashtag_count = HashtagCount.first_or_new({date: date, hashtag: tag}, {count: 0})
+    hashtag_count.count += nodes.count
+    hashtag_mh_count = HashtagMhCount.first_or_new({date: date, hashtag: tag}, {count: 0})
+    hashtag_mh_count.count += Sample.count(node: nodes.to_a)
+    hashtag_count.save
+    hashtag_mh_count.save
+  end
+  puts "Finished processing hashtags"
   STDOUT.flush
 end
