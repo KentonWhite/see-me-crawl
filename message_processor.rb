cba03c4c999d0cf2f7228e27fc12ecd2759b1d01
Kenton White <jkentonwhite@gmail.com>
2012-09-14 13:36:12 -0400

    Added process messages

--- message_processor.rb
+++ message_processor.rb
@@ -1,39 +1,36 @@
 require './lib/no_converge_sample.rb'
 require './lib/hashtag_twitter_node.rb'  
+require './lib/tag_counter'
 
 require 'amqp'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'])
-
-
-DataMapper.auto_upgrade!
-
-AMQP.start('amqp://lcpdyzjs:nko1XmnZfRul4Hza@gqezbdhq.heroku.srs.rabbitmq.com:21146/gqezbdhq') do |connection|
-  channel    = AMQP::Channel.new(connection, :auto_recovery => true)
-
-  channel.prefetch(1)
+def process_messages(messages)
+  counter = TagCounter.new
   
-  channel.queue("com.girih.samples", :durable => true, :auto_delete => false).subscribe(:ack => true) do |metadata, node|
-    case metadata.type
-    when "new_samples"
-      puts "Process new node #{node}"
-      STDOUT.flush
-      unless UnprocessedMessage.count(node: node) > 0 
-        node = HashtagTwitterNode.new(node.to_i)
-        node.check_hashtag
-        hashtags = UnprocessedHashtag.all
-        process_hashtags if hashtags.count > 1000
-
-      end
-    else
-      puts "Unknown message type #{metadata.type}"
+  if messages.count > 1000 then
+    puts "Processing #{messages.count} messages"
+    STDOUT.flush
+  
+    messages.each do |m|
+      counter.add(m.message_date, 'message', m.node)
+      attrs = m.attributes
+      attrs.delete(:id)
+      Message.create(attrs)    
+      m.destroy
     end
-    
-    metadata.ack
+  
+    counter.each do |date, tag, nodes|
+      message_count = MessageCount.first_or_new({date: date}, {count: 0})
+      message_count.count += nodes.count
+      message_mh_count = MessageMhCount.first_or_new({date: date}, {count: 0})
+      message_mh_count.count += Sample.count(node: nodes.to_a)
+      message_count.save
+      message_mh_count.save
+    end
+    puts "Finished processing messages"
+    STDOUT.flush
   end
   
-  puts "Ready to listen to messages queue"
-  STDOUT.flush
 end
 
 def process_hashtags(hashtags)
@@ -61,3 +58,39 @@ def process_hashtags(hashtags)
   puts "Finished processing hashtags"
   STDOUT.flush
 end
+
+DataMapper.setup(:default, ENV['DATABASE_URL'])
+
+
+DataMapper.auto_upgrade!
+
+AMQP.start('amqp://lcpdyzjs:nko1XmnZfRul4Hza@gqezbdhq.heroku.srs.rabbitmq.com:21146/gqezbdhq') do |connection|
+  channel    = AMQP::Channel.new(connection, :auto_recovery => true)
+
+  channel.prefetch(1)
+  
+  channel.queue("com.girih.samples", :durable => true, :auto_delete => false).subscribe(:ack => true) do |metadata, node|
+    case metadata.type
+    when "new_samples"
+      puts "Process new node #{node}"
+      STDOUT.flush
+      unless UnprocessedMessage.count(node: node) > 0 
+        node = HashtagTwitterNode.new(node.to_i)
+        node.check_hashtag
+        messages = UnprocessedMessage.all
+        process_messages(messages) if messages.count > 1000
+        hashtags = UnprocessedHashtag.all
+        process_hashtags(hashtags) if hashtags.count > 1000
+
+      end
+    else
+      puts "Unknown message type #{metadata.type}"
+    end
+    
+    metadata.ack
+  end
+  
+  puts "Ready to listen to messages queue"
+  STDOUT.flush
+end
+
